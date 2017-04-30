43b1c85dcbbea0cdf2c95460f5a594bc07e9e6a7
MaxMak <maxmak@MaxMak.local>
2017-04-30 21:54:03 +0800

    create

---
+++ OCPackage/OCPackage.h
@@ -0,0 +1,19 @@
+//
+//  OCPackage.h
+//  OCPackage
+//
+//  Created by MaxMak on 30/04/2017.
+//  Copyright © 2017 MaxMak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OCPackage.
+FOUNDATION_EXPORT double OCPackageVersionNumber;
+
+//! Project version string for OCPackage.
+FOUNDATION_EXPORT const unsigned char OCPackageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OCPackage/PublicHeader.h>
+
+
